e62718df825e57f22a5746d6a994ded5ac6edfd0
mllkvb <m_baibolat@kbtu.kz>
2024-01-22 23:07:28 +0600

    Intro

--- pp2/lab1/4.py
+++ pp2/lab1/4.py
@@ -1,4 +1,35 @@
-x=7
+"""CREATING VARIABLES"""
+x=5
 y="hello,world"
 print(x)
 print(y)
+
+#2nd example
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+"""CASTING"""
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+"""GET THE TYPE"""
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+"""SINGLE OR DOUBLE QUOTES"""
+x = "John"
+x = 'John'
+print(x)#they are same
+
+"""CASE-SENSITY"""
+a = 4
+A = "Sally"
+print(a)
+print(A) #two different variables
--- pp2/lab1/5.py
+++ pp2/lab1/5.py
@@ -1,2 +1,14 @@
-print("hello,world")
-#This is a comment
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
