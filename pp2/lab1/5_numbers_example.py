d0073cf6c9c2bce593db66df9778c7dbffeb62eb
mllkvb <156894859+mllkvb@users.noreply.github.com>
2024-01-26 22:51:27 +0600

    1

---
+++ pp2/lab1/5_numbers_example.py
@@ -0,0 +1,48 @@
+#int
+x=5
+y=1892
+z=-1892
+print(type(x))
+print(type(y))
+print(type(z))
+
+#float
+x=1.892
+y=18.92
+z=-18.92
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+#complex
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#type conversion
+x=1
+y=7.7
+z=3j
+a=float(x)
+b=int(y)
+c=complex(z)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+#RANDOM NUMBER
+import random
+print(random.randrang(2.30))
